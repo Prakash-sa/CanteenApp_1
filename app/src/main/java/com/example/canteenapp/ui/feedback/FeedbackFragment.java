017182ff7691125f225a4a21f8bf4de359e3e190
Prakash-sa <sainiprakash525@gmai.com>
2020-02-01 12:24:57 +0530

    Feedback done.

--- app/src/main/java/com/example/canteenapp/ui/feedback/FeedbackFragment.java
+++ app/src/main/java/com/example/canteenapp/ui/feedback/FeedbackFragment.java
@@ -52,39 +52,56 @@ public class FeedbackFragment extends Fragment {
                 radioGroup=root.findViewById(R.id.radio_answer_1);
                 int selected_id_1=radioGroup.getCheckedRadioButtonId();
                 radioButton=root.findViewById(selected_id_1);
+                if(radioButton.getText()!=null)
                 radioanswer.put("Food Quality",radioButton.getText().toString());
+                else
+                    radioanswer.put("Food Quality","Good");
 
                 radioGroup=root.findViewById(R.id.radio_answer_2);
                 selected_id_1=radioGroup.getCheckedRadioButtonId();
                 radioButton=root.findViewById(selected_id_1);
+                if(radioButton.getText()!=null)
                 radioanswer.put("Food Quantity",radioButton.getText().toString());
+                else
+                    radioanswer.put("Food Quantity","Good");
 
                 radioGroup=root.findViewById(R.id.radio_answer_3);
                 selected_id_1=radioGroup.getCheckedRadioButtonId();
                 radioButton=root.findViewById(selected_id_1);
+                if(radioButton.getText()!=null)
                 radioanswer.put("Mess are Hygienic",radioButton.getText().toString());
+                else
+                    radioanswer.put("Mess are Hygienic","Good");
 
-
-                //akdsj
                 radioGroup=root.findViewById(R.id.radio_answer_4);
                 selected_id_1=radioGroup.getCheckedRadioButtonId();
                 radioButton=root.findViewById(selected_id_1);
+                if(radioButton.getText()!=null)
                 radioanswer.put("Food Hygienic",radioButton.getText().toString());
+                else
+                    radioanswer.put("Food Hygienic","Good");
 
                 radioGroup=root.findViewById(R.id.radio_answer_5);
                 selected_id_1=radioGroup.getCheckedRadioButtonId();
                 radioButton=root.findViewById(selected_id_1);
+                if(radioButton.getText()!=null)
                 radioanswer.put("Utensils Hygienic",radioButton.getText().toString());
+                else
+                    radioanswer.put("Utensils Hygienic","Good");
 
                 radioGroup=root.findViewById(R.id.radio_answer_6);
                 selected_id_1=radioGroup.getCheckedRadioButtonId();
                 radioButton=root.findViewById(selected_id_1);
+                if(radioButton.getText()!=null)
                 radioanswer.put("Food Price",radioButton.getText().toString());
+                else
+                    radioanswer.put("Food Price","Good");
+
 
                 if(feedback_result!=null){
                     Toast.makeText(getContext(),"Success",Toast.LENGTH_LONG).show();
                     Feedback feedback=new Feedback(feedback_result,radioanswer);
-                    myRef.child("Feedback").setValue(feedback);
+                    myRef.child("Feedback").child("mess_id").setValue(feedback);
                 }
             }
         });
