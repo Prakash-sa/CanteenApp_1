6fbdc9b6b47f340dc715c71677674e8037da4193
Prakash-sa <sainiprakash525@gmail.com>
2020-02-04 19:54:19 +0530

    Feedback of mess done.

---
+++ app/src/main/java/com/example/canteenapp/ui/mess/feedback/Feedback.java
@@ -0,0 +1,40 @@
+package com.example.canteenapp.ui.mess.feedback;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.canteenapp.R;
+
+public class Feedback extends Fragment {
+
+    private FeedbackViewModel mViewModel;
+
+    public static Feedback newInstance() {
+        return new Feedback();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View v=inflater.inflate(R.layout.feedback_fragment, container, false);
+
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(FeedbackViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
